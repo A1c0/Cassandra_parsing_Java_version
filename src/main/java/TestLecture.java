500be7e020a46d425edc617612c03454cdddaaba
Alexandre CONTASSOT <alexandrecontassot@hotmail.fr>
2018-03-11 20:40:31 +0100

    Creation d'un tableau 2D pour stocker les elements des requetes

--- src/main/java/TestLecture.java
+++ src/main/java/TestLecture.java
@@ -1,22 +1,26 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class TestLecture {
     public static void main(String[] args) throws FileNotFoundException {
+
+        ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+
+
         Scanner scan = new Scanner(new File("donnee.txt"));
-        int line = 1;
+        int line = 0;
         while (scan.hasNextLine()) {
             String lines = scan.nextLine();
             if (lines.equals(""))
                 continue; // permet de supprimer les lignes superflues
             System.out.println("Request " + line + ": " + lines);
+            int word = 0;
             Scanner scanString = new Scanner(lines);
-            int word = 1;
-            System.out.println("==============================");
-            System.out.println("===== Affichage des mots =====");
-            System.out.println("==============================");
             System.out.println();
-
+            System.out.println("===== Affichage des mots =====");
+            ArrayList<String> requete = new ArrayList<String>();
             while (scanString.hasNext()) {
                 String mots = scanString.next();
                 if (mots.charAt(0) == '\"') {
@@ -31,9 +35,18 @@ public class TestLecture {
                 if (mots.charAt(0) == '"')
                     mots = mots.substring(1, mots.length() - 1);
                 System.out.println("Mot " + word + ": " + mots);
+                requete.add(mots);
                 word++;
             }
+            data.add(requete);
             line++;
+            System.out.println();
+        }
+        for (ArrayList<String> als : data) {
+            System.out.println("===== requeste =====");
+            for (String s : als) {
+                System.out.println(s);
+            }
         }
     }
 }
