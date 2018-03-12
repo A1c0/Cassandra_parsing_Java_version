be71311cff504a58768dbea906c8e20427dae75e
alexConts <alexandrecontassot@hotmail.fr>
2018-03-12 23:36:20 +0100

    creation de la requete concernant les elements invasifs et remarcable

--- src/main/java/TestLecture.java
+++ src/main/java/TestLecture.java
@@ -48,6 +48,26 @@ public class TestLecture {
             //requete pour l'habitat
             String requete = "INSERT INTO habitat(id, milieu, typeH, photo, localisation) VALUES(uuid(), '" + als.get(0) + "', '" + als.get(1) + "', '" + als.get(2) + "', '" + als.get(3) + " " + als.get(4) + " " + als.get(5) + " " + als.get(6)+"');";
             System.out.println(requete);
+            int iterator  = 7;
+            while (iterator < als.size()){
+                if (als.get(iterator + 1).charAt(0) == 'i'){ //GESTIONS DES ELEMENTS INVASIFS
+                    System.out.println("Insertion d'un élément invasifs");
+                    requete = "INSERT INTO element_invasif(id, idHab, type, photo, localisation) VALUES(uuid(), uuid(), '" + als.get(iterator) + "', '" + als.get(iterator+1) +  "', '" + als.get(iterator+2) + " " + als.get(iterator+3) + " " + als.get(iterator+4) + " " + als.get(iterator+5)+"');";
+                    System.out.println(requete);
+                }
+                if (als.get(iterator + 1).charAt(0) == 'r'){ // GESTIONS DES ELEMENTS REMARCABLE
+                    requete = "INSERT INTO element_invasif(id, idHab, type, photo, localisation) VALUES(uuid(), uuid(), '" + als.get(iterator) + "', '" + als.get(iterator+1) +  "', '" + als.get(iterator+2) + " " + als.get(iterator+3) + " " + als.get(iterator+4) + " " + als.get(iterator+5)+"');";
+                    System.out.println("Insertion d'un élément remarcable");
+                    System.out.println(requete);
+
+                }
+                if (als.get(iterator + 1).charAt(0) == 'E'){ // GESTIONS DES ESPECES REMARCABLES
+                    System.out.println("Insertion d'une espèce");
+                    System.out.println("pas encore implementé dans la base");
+
+                }
+                iterator += 6;
+            }
         }
     }
 }
